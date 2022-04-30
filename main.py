5443966fad0f7329233d72445d62e21fbbe63796
Lucas Costa de Andrade <81720478+MetallonX@users.noreply.github.com>
2022-04-30 00:13:05 -0300

    Update main
    Update adicionando o endpoint de visualização de cidades vizinhas e o começo do endpoint de construção de rotas

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
 #DESAFIO DEV JR - MILENIO CAPITAL#
 
 #AUTHOR: LUCAS COSTA DE ANDRADE#
+from fastapi import FastAPI, Path
+from modeloGraph import payload
+from modeloGraph import calcRoutes, createAdjNodes
 
-from fastapi import FastAPI , Path
-from typing import Optional, List
-import json
-
-from sqlalchemy import null
-from modeloGraph import  payload
-
-graphBD : payload = []
+graphBD: payload = []
 
 app = FastAPI()
+
+#ENDPOINTS#
+
 
 @app.get("/descricaoAPI")
 def desc():
@@ -21,14 +20,16 @@ def desc():
 #Retorna todos os grafos salvos até o momento#
 @app.get("/retornagrafos")
 def retornagrafos():
-    if  (graphBD == []):
+    if (graphBD == []):
         return "Não tem nenhum grafo registrado!"
-    else:    
+    else:
         return graphBD
 
 #recebe as rotas do grafo e salva com o id do grafo#
+
+
 @app.post("/graph")
-def pegaID(graph:payload):
+def pegaID(graph: payload):
     if(graphBD):
         graph.id = graphBD[-1].id + 1
     else:
@@ -36,22 +37,36 @@ def pegaID(graph:payload):
     graphBD.append(graph)
     return graphBD[-1]
 
+#busca um grafo salvo anteriormente#
+
 
 @app.get("/graph/{graphId}")
-def returnGraph(graphId: int):
-    if  (graphBD == []):
-        return "HTTP NOT FOUND"  
-    for i in graphBD: #Lê os dicionários da lista/BD#
+def returnGraph(graphId: int = Path(None, description="Digite o ID do grafo que está buscando")):
+    if (graphBD == []):
+        return "HTTP NOT FOUND"
+    for i in graphBD:  # Lê os dicionários da lista/BD#
         if(graphId == i.id):
             return i
         elif i == graphBD[-1]:
             return "HTTP NOT FOUND"
 
-@app.post("/routes/{graphId}/from/{town1}/to/{town2}")
-def routes(graphId: int,town1: str,town2: str):
-    
-    
+#verifica as rotas disponíveis entre origem e destino, com limite de paradas opcional#
 
-# @app.get("/graph/{graphID}")
-#     def get_graph(graphID : int):
-#         return Graph
+
+@app.get("/adjacentes/{graphId}")
+def returnAdjacents(graphId: int):
+    for i in graphBD:
+        if(graphId == i.id):
+            return createAdjNodes(i)
+
+
+@app.post("/routes/{graphId}/from/{town1}/to/{town2}")
+def routes(graphId: int, town1: str, town2: str):
+    if (graphBD == []):
+        return "HTTP NOT FOUND"
+    for i in graphBD:
+        if(graphId == i.id):
+            graph = createAdjNodes(i)
+            return calcRoutes(graph, town1, town2)
+        elif i == graphBD[-1]:
+            return "HTTP NOT FOUND"
