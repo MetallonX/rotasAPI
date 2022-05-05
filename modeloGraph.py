544cbaa29391ab041d0d0b167226c3acb7d36d74
Lucas Costa de Andrade <81720478+MetallonX@users.noreply.github.com>
2022-05-05 19:21:05 -0300

    Update modeloGraph.py
    Commit com update final com todas as funcionalidades requisitadas.

--- modeloGraph.py
+++ modeloGraph.py
@@ -1,4 +1,7 @@
+from collections import defaultdict
+from turtle import distance
 from typing import List, Optional
+from numpy import Infinity
 from pydantic import BaseModel
 
 
@@ -13,7 +16,7 @@ class payload(BaseModel):
     data: list[modeloRota]
 
 
-class routeBase(dict):
+class routeBase(dict):  # diiconario de uma rota#
     def __init__(self):
         self = dict()
 
@@ -21,7 +24,7 @@ class routeBase(dict):
         self[key] = value
 
 
-class routesBig(BaseModel):
+class routesBig(defaultdict):
     routes: list[routeBase]
 
 
@@ -33,26 +36,48 @@ class adjList(dict):  # cria um dicionario com keys de cada cidade e valores das
         self[key] = value
 
 
+class shortestPath(defaultdict):  # dicionario responsável pelo caminho mais curto#
+    distance: Optional[int]
+    path: list
+
+
 #FUNÇÕES DE CÁLCULO DE ROTA#
 
-
-def calcRoutes(graph: adjList, source: str, target: str, route: Optional[routeBase] = {}, routes: Optional[routesBig] = [], path: Optional[str] = ''):
-    route = routeBase()
-    routes: routesBig = []
-    path = path + source
-    for neighbour in graph[source]:
-        path = path + neighbour
-        graph[source].remove(neighbour)
-        if(path[-1] == target):
-            route.add("route", path)
-            route.add("stops", len(path)-1)
-            routes.append(route)
-        calcRoutes(graph, neighbour, target, route, routes, path)
-    return routes
-
+class calc_rotas:
+    def getRoutes(graph: adjList, source: str, path: str, target: str, maxStops: Optional[int]) -> routesBig:
+        def dfs(graph: adjList, source: str, path: str, target: str, maxStops: Optional[int]):
+            route = routeBase()
+            if source in graph.keys():
+                for node in graph[source]:
+                    if node == target:
+                        temp = [x for x in path]
+                        temp.append(node)
+                        s = ''.join(temp)
+                        if(maxStops != None):
+                            if((len(originalSource+s)-1) <= maxStops):
+                                route.add("route", originalSource+s)
+                                route.add("stops", len(originalSource+s)-1)
+                                routes["routes"].append(route)
+                        else:
+                            route.add("route", originalSource+s)
+                            route.add("stops", len(originalSource+s)-1)
+                            routes["routes"].append(route)
+                    else:
+                        if(node not in path):
+                            path = path+node
+                            dfs(graph, node, path, target, maxStops)
+                            path = path.rstrip(path[-1])
+            else:
+                return routes
+        originalSource = source
+        routes = routesBig(routes=[])
+        dfs(graph, source, path, target, maxStops)
+        return routes
 
 #CRIA UM GRAFO ONDE SE PEGA A CIDADE E SEU VALOR É UMA LISTA COM AS CIDADES VIZINHAS#
-def createAdjNodes(graph: payload):
+
+
+def createAdjNodes(graph: payload) -> adjList:
     nodesAndAdj = adjList()
     for i in graph.data:
         if(i.source not in nodesAndAdj.keys()):
@@ -61,3 +86,24 @@ def createAdjNodes(graph: payload):
                 if y.source in nodesAndAdj.keys() and y.target not in nodesAndAdj[y.source]:
                     nodesAndAdj[y.source].append(y.target)
     return nodesAndAdj
+
+
+#FUNÇÃO DE CÁLCULO DA ROTA MAIS CURTA DADO UMA CIDADE ORIGEM E UMA  DESTINO#
+def calcShortestPath(routes: routesBig, graph: payload) -> shortestPath:
+    path = ''
+    distance = 0
+    shortPath = shortestPath(distance=Infinity, path=[])
+    for i in routes["routes"]:
+        nodes = [x for x in i["route"]]
+        countNodes = 0
+        for j in nodes:
+            countNodes = countNodes + 1
+            path = path + j
+            if(countNodes < len(nodes)):
+                for k in graph.data:
+                    if k.source == path[countNodes - 1] and k.target == nodes[countNodes]:
+                        distance = k.distance + distance
+                        if(shortPath["distance"] > distance and (countNodes+1) == len(nodes)):
+                            shortPath["distance"] = distance
+                            shortPath["path"] = nodes
+    return shortPath
